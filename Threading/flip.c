9dc5645dc0150a4e3841614bbeef3d78baea8e1e
StefanHeijden <32574125+StefanHeijden@users.noreply.github.com>
2019-12-15 20:34:23 +0100

    Start
    Started on the assigment, Made sort of template for all methods and such. Implemented the method to flip bits.

--- Threading/flip.c
+++ Threading/flip.c
@@ -2,8 +2,8 @@
  * Operating Systems {2INCO} Practical Assignment
  * Threaded Application
  *
- * STUDENT_NAME_1 (STUDENT_NR_1)
- * STUDENT_NAME_2 (STUDENT_NR_2)
+ * Stefan van der Heijden 0910541
+ * Sonya Zarkova 1034611
  *
  * Grading:
  * Students who hand in clean code that fully satisfies the minimum requirements will get an 8. 
@@ -21,13 +21,92 @@
 #include "uint128.h"
 #include "flip.h"
 
+int currentNoOfThreads = 0;
+
+
+// Print for each bit whether it is flipped or not
+void printBits(){
+	for(int i = 1; i <= NROF_PIECES; i = i + 1) {
+		if (BIT_IS_SET (buffer[(i/128) + 1], i)) {
+			printf ("bit %d is set\n", i);
+		}else {
+			printf ("bit %d is not set\n", i);
+		}
+	}
+}
+
+
+// Create thread for current piece --> thread_test()
+void createThread(int piece){
+	printf ("Created thread for %d\n", piece);
+	// Create thread for current piece
+	// TO DO <-----
+	// Then try flip all bits
+	flipAllBits(piece);
+	printBits(); // TEST <-----
+	// Close thread
+	// TO DO <-----
+	// Lower number of current Threads
+	currentNoOfThreads = currentNoOfThreads - 1;
+	printf ("Closed thread for %d\n", piece);
+}
+
+
+// Flip all bits for ceratin multiplier --> thread_mutex_test()
+void flipAllBits(int piece){
+	int pointer = 1;
+	int currentPosition = piece;
+	// For element in the list that has not been flipped:
+	while(currentPosition <= NROF_PIECES){
+		// Check mutexlock for piece
+		// TO DO <-----
+		// If free, lock it
+		// TO DO <-----
+		// then flip bit --> flitbit(piece)
+		flipBit(currentPosition);
+		// Then free lock
+		// TO DO <-----
+		// Then remove element from list
+		// TO DO <-----
+		// If not then go to next element in the list
+		pointer = pointer + 1;
+		currentPosition = pointer * piece;
+	}
+}
+
+
+// Flip bit of given piece --> bit_test()
+void flipBit(int currentPosition){
+	if(BIT_IS_SET(buffer[(currentPosition/128) + 1], currentPosition % 128)){
+		BIT_CLEAR(buffer[(currentPosition/128) + 1], currentPosition % 128);
+	}else{
+		BIT_SET(buffer[(currentPosition/128) + 1], currentPosition % 128);
+	}
+}
+
+
+// TODO: start threads to flip the pieces and output the results
+// (see thread_test() and thread_mutex_test() how to use threads and mutexes,
+//  see bit_test() how to manipulate bits in a large integer)
 int main (void)
 {
-    // TODO: start threads to flip the pieces and output the results
-    // (see thread_test() and thread_mutex_test() how to use threads and mutexes,
-    //  see bit_test() how to manipulate bits in a large integer)
-
-
+	// Output the start
+	printBits();
+	int currentPiece = 1;
+	// Create thread for each piece
+	while(currentPiece <= NROF_PIECES){
+		// Create thread for current piece
+		// but only if there are more threads available
+		if(currentNoOfThreads < NROF_THREADS) {
+			currentNoOfThreads = currentNoOfThreads + 1;
+			createThread(currentPiece);
+			currentPiece = currentPiece + 1;
+		}else{
+			break;
+		}
+	}
+	// Output the results
+	printBits();
     return (0);
 }
 
--- Threading/flip.h
+++ Threading/flip.h
@@ -14,13 +14,25 @@
 /**
  * NROF_PIECES: size of the board; number of pieces to be flipped
  */
-#define NROF_PIECES			3000
+#define NROF_PIECES			3
 
 /**
  * NROF_THREADS: number of threads that can be run in parallel
  * (value must be between 1 and ... (until you run out of system resources))
  */
 #define NROF_THREADS		10
+
+// create a bitmask where bit at position n is set
+#define BITMASK(n)          (((uint128_t) 1) << (n))
+
+// check if bit n in v is set
+#define BIT_IS_SET(v,n)     (((v) & BITMASK(n)) == BITMASK(n))
+
+// set bit n in v
+#define BIT_SET(v,n)        ((v) =  (v) |  BITMASK(n))
+
+// clear bit n in v
+#define BIT_CLEAR(v,n)      ((v) =  (v) & ~BITMASK(n))
 
 /**
  * buffer[]: datastructure of the pieces; each piece is represented by one bit
